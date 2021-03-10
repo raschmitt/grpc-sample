eda910f819abf280786323305a609fecc2405b1e
Rogerio Schmitt <rogerio.schmitt@me.com>
2021-03-09 21:26:31 -0300

    Initial commit

---
+++ grpc-sample/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Service;
+
+namespace Client
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            var httpHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+
+            var channel = GrpcChannel.ForAddress("https://grpc-service",
+                new GrpcChannelOptions { HttpHandler = httpHandler });
+            var client = new Greeter.GreeterClient(channel);
+            
+            var helloRequest = new HelloRequest
+            {
+                Name = "GreeterClient"
+            };
+            
+            var reply = await client.SayHelloAsync(helloRequest);
+            
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ grpc-sample/Service/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Service.Services;
+
+namespace Service
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/health");
+                endpoints.MapGrpcService<GreeterService>();
+            });
+        }
+    }
+}
